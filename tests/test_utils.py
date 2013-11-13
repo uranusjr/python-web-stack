7ae50554c49e2d0f5d37beef818e709bcb6f37f3
Tzu-ping Chung <uranusjr@gmail.com>
2013-11-13 18:22:23 +0800

    Some more tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 import os.path
-from nose.tools import ok_, eq_, raises
+from nose.tools import eq_, assert_raises, assert_true, raises
 from pywebstack.formulae import Formula
 from pywebstack import utils
 from . import ALL_FORMULAE_NAMES
@@ -12,17 +12,34 @@ def test_normalize():
     eq_(utils.normalize('..', 'tests', ), os.path.dirname(__file__))
 
 
+def test_environment_object():
+    env = utils._Environment({'answer': 42})
+    eq_(env.answer, 42)
+    eq_(env['answer'], 42)
+    with assert_raises(AttributeError):
+        env.fail
+    with assert_raises(KeyError):
+        env['fail']
+    env.fail = 'epic'
+    eq_(env['fail'], 'epic')
+
+
 def test_chdir():
     current = os.getcwd()
     outer = os.path.abspath(utils.normalize(current, '..'))
     with utils.chdir('..'):
         eq_(os.getcwd(), outer)
     eq_(os.getcwd(), current)
+    try:
+        with(utils.chdir('you_shall_not_pass')):
+            assert_true(False)      # Shouldn't enter here
+    except OSError:
+        eq_(os.getcwd(), current)
 
 
 def test_get_formula_class():
     for name in ALL_FORMULAE_NAMES:
-        ok_(issubclass(utils.get_formula_class(name), Formula))
+        assert_true(issubclass(utils.get_formula_class(name), Formula))
 
 
 @raises(utils.UnrecognizedFormulaError)
@@ -33,7 +50,7 @@ def test_get_formula_class_fail():
 def test_get_formula():
     for name in ALL_FORMULAE_NAMES:
         formula = utils.get_formula(name, 'test_project')
-        ok_(isinstance(formula, Formula))
+        assert_true(isinstance(formula, Formula))
 
 
 if __name__ == '__main__':
